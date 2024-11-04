bcd6f1bad3206d4925f7f56e51c7bf666b0ec186
AngieInes <MVioli814@alumnos.frh.utn.edu.ar>
2024-11-04 01:22:34 -0300

    pagina web definitiva

---
+++ js.js
@@ -0,0 +1,10 @@
+
+const menu = document.querySelector("#menu");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+menu_bar.addEventListener(click, () => {
+    menu.classList.add("visible");
+})
+
+
